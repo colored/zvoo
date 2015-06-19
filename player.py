29ab8f591bd315ae4f650f4ada9ba0603fced878
colored <300000001mps@gmail.com>
2015-06-19 13:46:31 -0500

    track will be deleted after N times

--- player.py
+++ player.py
@@ -6,8 +6,10 @@ from track import Track
 
 
 def play(track):
+    track.plays += 1
     time.sleep(3)
     print track.path
+    print track.plays
 
 
 def getTrackList():
@@ -16,19 +18,20 @@ def getTrackList():
     for root, subdirs, files in os.walk(rootdir):
         for file in files:
             if file.endswith(".mp3"):
-                try:
-                    tracks.append(Track(os.path.join(root, file)))
-                except TypeError:
-                    pass
-
+                tracks.append(Track(os.path.join(root, file)))
+    # encoded = json.dumps(tracks)
     return tracks
 
 
 def main():
     tracklist = getTrackList()
-    random.shuffle(tracklist)
-    for track in tracklist:
-        play(track)
+    while (tracklist):
+        current_track = random.choice(tracklist)
+        if (current_track.plays == 3):
+            tracklist.remove(current_track)
+            print str(current_track) + " REMOVED ! ! !"
+            continue
+        play(current_track)
 
 
 main()
--- track.py
+++ track.py
@@ -4,7 +4,21 @@ __author__ = 'azubko'
 class Track:
     def __init__(self, path):
         self._path = path
+        self._plays = 0
+
+    def __str__(self):
+        return self._path
 
     @property
     def path(self):
         return self._path
+
+    @property
+    def plays(self):
+        return self._plays
+
+    @plays.setter
+    def plays(self, value):
+        self._plays += 1
+
+    __repr__ = __str__
